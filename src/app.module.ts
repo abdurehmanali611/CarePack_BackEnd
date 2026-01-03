386022516f9ce3be94103bbb79478c9aebe986e5
abdurehmanali611 <abdurehmanali611@gmail.com>
2026-01-03 11:34:38 +0300

    Done

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { medicalModule } from './Medical/medical.module';
 import { credentialModule } from './credential/credential.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/CarePack'), UserModule, medicalModule, credentialModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://abdurehmanali611:35000642@cluster0.nk4lg.mongodb.net/CarePack?retryWrites=true&w=majority'), UserModule, medicalModule, credentialModule],
   controllers: [],
   providers: [],
 })
