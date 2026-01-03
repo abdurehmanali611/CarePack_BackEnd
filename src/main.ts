5f8fdc34ed70f13968f9633e2038c631b29a6cd0
abdurehmanali611 <abdurehmanali611@gmail.com>
2026-01-03 12:49:33 +0300

    final of final

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   },
   }))
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: ['https://care-pack-red.vercel.app','http://localhost:3000', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
   })
   await app.listen(process.env.PORT ?? 4000);
